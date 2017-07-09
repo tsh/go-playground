1409c1a9ba7b8d1bcde0f516083151564eb69b6d
tsh <dr.tallin@gmail.com>
2017-07-09 13:50:47 +0300

    read from channel

---
+++ chan2.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"fmt"
+	"strings"
+)
+
+func main(){
+	phrase := "These are the times that try men's souls\n"
+
+	words := strings.Split(phrase, " ")
+
+	ch := make(chan string, len(words))
+
+	for _, word := range words {
+		ch <- word
+	}
+
+	close(ch)
+
+	for msg := range ch {
+		fmt.Print(msg + " ")
+	}
+}
